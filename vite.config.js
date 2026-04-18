187758f8b769a96b056b78ea41af2d5a67ae8dc6
100wahidi <wahidi.mouad@student.emi.ac.ma>
2026-04-18 17:03:17 +0200

    intial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/r/"
+  base: "/r/",
+  build: {
+    outDir: "docs" // Output build files to /docs folder
+  }
 })
